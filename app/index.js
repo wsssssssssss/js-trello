e3026b97d3efea1da8dd4227ab380391137d7259
kimjuneseo <june3929213512@gmail.com>
2022-01-26 16:49:38 +0900

    cardAdd

--- app/index.js
+++ app/index.js
@@ -1,86 +1,66 @@
-const popupWrap = document.querySelector(".list__popup");
-const cancelBtn = document.querySelector(".cancel__btn");
 const addlistBtn = document.querySelector(".addlist");
+let boardContainer = document.querySelector(".container");
+// popuplist
+const list_popupWrap = document.querySelector(".list__popup");
+const list_exitBtn = document.querySelector(".list__cancel--btn");
+const list_addBtn = document.querySelector(".list__add--btn");
+const list_listForm = document.list__form;
+//popupcard
+const card_popupWrap = document.querySelector(".card__popup");
+const card_exitBtn = document.querySelector(".card__cancel--btn");
+const card_addBtn = document.querySelector(".card__add--btn");
 
-const boardContainer = document.querySelector(".container");
-const registrationListBtn = document.querySelector(".add__btn");
-const addInput = document.querySelector(".list__input input");
-const popupToggle = () => popupWrap.classList.toggle("none");
+const list_popupToggle = () => list_popupWrap.classList.toggle("none");
+const card_popupToggle = () => card_popupWrap.classList.toggle("none");
+list_exitBtn.addEventListener("click", list_popupToggle);
+addlistBtn.addEventListener("click",list_popupToggle);
 
+card_exitBtn.addEventListener("click", card_popupToggle)
+card_addBtn.addEventListener("click", card_popupToggle)
 
 const addList = () => {
-    let title = addInput.value;
-    const list = document.createElement('div');
-    list.classList.add('list', 'grid');
-    list.innerHTML +=
-    `
-        <div class="list__container">
-          <div class="list__header flex">
-            <p class="list__title">${title}</p>
-            <div class="list__menu"><i class=" fa-ellipsis-h fa"></i></div>
-          </div>
-          <div class="cards grid">
-          </div>
-          <div class="list__footer flex">
-            <div class="list__footer--txt">+ Add a card</div>
-            <i class="fa fa-copy"></i>
-          </div>
-        </div>
-    `;
-    boardContainer.insertBefore(list, boardContainer.firstChild);
-};
-registrationListBtn.addEventListener("click", addList);
-cancelBtn.addEventListener("click", popupToggle);
-addlistBtn.addEventListener("click",() => {
-    popupToggle()
-    console.log(123123);
-});
+  let title = list_listForm.list.value;
+  const list = document.createElement('div');
+  list.classList.add('list', 'grid');
+  list.innerHTML +=
+  `
+  <div class="list__container">
+  <div class="list__header flex">
+  <p class="list__title">${title}</p>
+  <div class="list__menu"><i class=" fa-ellipsis-h fa"></i></div>
+  </div>
+  <div class="cards grid">
+  </div>
+  <div class="list__footer flex">
+  <div class="list__footer--txt">+ Add a card</div>
+  <i class="fa fa-copy"></i>
+  </div>
+  </div>
+  `;
+  addlistBtn.before(list);
+  list_listForm.reset();
+  list_popupToggle();
+  
+  boardContainer.childNodes.forEach(el => {
+    el.addEventListener("click", (e) => {
+      if(e.target.classList.contains("list__footer")){
+        console.log(e.target.parentNode.childNodes[3]);
 
-if (window.indexedDB) {
-    let name = 'Trello';
-    let version = 1;
-    let db = null;
-    let request = indexedDB.open(name, version);
-    // db가 생성될때 트리거 되거나 버전이 업데이트 되었을떄 트리거됨
-    request.onupgradeneeded = function(event) { 
-    console.log('onupgradeneeded');
-      db = request.result;
-      let key = "ids";
-      let name = 'store name';
-      let store = db.createObjectStore(name, { keyPath: key });
-      console.log(store);
-      let indexName = 'by_name';
-      let keyPath = 'name';
-      let index = store.createIndex(indexName, keyPath);
-      let obj = {
-        [key]: 2,
-        [keyPath]: "앙기모띠"
       }
-      store.put(obj);
-    };
-    
-    request.onsuccess = function(event) {
-        console.log(event.target);
-      db = request.result;
-      // IDBTransaction
-      let transaction = db.transaction(['store name'], "readonly");
-      // IDBObjectStore
-      let objectStore = transaction.objectStore('store name');
-      // IDBRequest
-      let cursor = objectStore.openCursor();
-        cursor.onsuccess = function(event) {
-          // IDBCursorWithValue
-          let cursor = event.target.result;
-          if ( cursor ) {
-            console.log(cursor)
-            cursor.continue();
-          } else {
-            console.log('end');
-          }
-        };
-    };
-  }
+    })
+   });
+};
+list_addBtn.addEventListener("click", addList);
 
-
-
-
+ 
+const openFileButton = document.querySelector("#openFile"); 
+openFileButton.addEventListener("click", async e => {
+  const [fileHandle] = await showOpenFilePicker();   
+  const file = await fileHandle.getFile();
+  const urlCreator = window.URL || window.webkitURL;
+  const mediaUrl = urlCreator.createObjectURL(file);
+  const image = document.querySelector("#card__add--img");
+  image.src = mediaUrl;
+  console.log(mediaUrl);
+  
+});
